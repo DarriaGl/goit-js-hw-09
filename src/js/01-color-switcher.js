926c26cd9478cb28e4750449db777a55682bf004
DarriaGl <gladchenkodasha13@gmail.com>
2023-06-08 17:51:16 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,12 +7,16 @@ const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 let intervalId = null;
 function startSwitcher() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
   intervalId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 }
+
 function stopSwitcher() {
   startBtn.disabled = false;
+  stopBtn.disabled = true;
   clearInterval(intervalId);
 }
 startBtn.addEventListener('click', startSwitcher);
